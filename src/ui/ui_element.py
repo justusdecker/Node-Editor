731f4070ab98042b735e6fb1e2150b323a35bbf0
Justus <justus.d2025@gmail.com>
2025-10-31 15:21:27 +0100

    added ui_element preset

---
+++ src/ui/ui_element.py
@@ -0,0 +1,21 @@
+
+"""
+For all the following:
+Copy the source code from my pygameEngine & refactor it to match the project requirements
+"""
+
+class UIManager:
+    
+    def __init__(self):
+        pass
+    #+ Rendering with offset
+    #+ Update in reverse
+
+class UIElement:
+    uid = 0
+    def __init__(self):
+        pass
+    
+class UIGroup:
+    def __init__(self):
+        pass
