63d3f13f48752501cea07c5e95cba4e3db4f33a4
Justus <justus.d2025@gmail.com>
2025-11-06 13:05:19 +0100

    changed the default ui to draggable & set the parent

--- ui_Test.py
+++ ui_Test.py
@@ -3,7 +3,7 @@ from src.ui.ui_element import UIM, UIElement
 from src.constants import *
 APP = App()
 
-A = UIElement(APP, Vector2(0,0),Vector2(15,15))
+A = UIElement(APP, Vector2(0,0),Vector2(15,15),draggable=True)
 B = UIElement(APP, Vector2(53,0),Vector2(15,15))
-C = UIElement(APP, Vector2(130,0),Vector2(15,15))
+C = UIElement(APP, Vector2(130,0),Vector2(15,15),parent=A)
 APP.run()
