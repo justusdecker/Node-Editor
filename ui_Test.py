538d8e54ae61f9c8d9a17bc2e8c8d94b9c487149
Justus <justus.d2025@gmail.com>
2025-11-07 02:56:00 +0100

    added textinput

--- ui_Test.py
+++ ui_Test.py
@@ -1,9 +1,10 @@
 from src.app import App
-from src.ui.ui_element import UIM, UIElement
+from src.ui.ui_element import UIM, UIElement, TextInput
 from src.constants import *
 APP = App()
 
 A = UIElement(APP, Vector2(0,0),Vector2(15,15),draggable=True)
 B = UIElement(APP, Vector2(53,0),Vector2(15,15))
 C = UIElement(APP, Vector2(130,0),Vector2(15,15),parent=A)
+D = TextInput(APP, Vector2(130,0),Vector2(15,15),parent=A)
 APP.run()
