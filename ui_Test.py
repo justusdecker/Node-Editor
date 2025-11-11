1ddb12e1ed4f32e44a7109a52ea2c02e39047373
Justus <justus.d2025@gmail.com>
2025-11-11 09:20:28 +0100

    fixed repeating keys

--- ui_Test.py
+++ ui_Test.py
@@ -2,11 +2,11 @@ from src.app import App
 from src.ui.ui_element import UIM, UIElement, TextInput, UIDropDown
 from src.constants import *
 APP = App()
-
+pg.key.set_repeat(500,50)
 A = UIElement(APP, Vector2(0,0),Vector2(15,15),draggable=True)
 B = UIElement(APP, Vector2(53,0),Vector2(15,15))
 C = UIElement(APP, Vector2(130,0),Vector2(15,15),parent=A)
-D = TextInput(APP, Vector2(130,0),Vector2(15,15),parent=A, multiline=True, max_length=4)
+D = TextInput(APP, Vector2(130,0),Vector2(200,100),parent=A, multiline=True, max_length=64)
 E = UIDropDown(APP, Vector2(0,0))
 E.set_subs(['ABC','DEF','GHI'])
 APP.run()
