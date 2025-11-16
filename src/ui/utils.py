7c3f7ebbe9477ab9c24477c49b5352a306d0001f
Justus <justus.d2025@gmail.com>
2025-11-16 14:25:29 +0100

    add spinbox to utils

--- src/ui/utils.py
+++ src/ui/utils.py
@@ -8,4 +8,5 @@ from src.ui.ui_textinput import UITextInput
 from src.ui.ui_dropdown import UIDropDown
 from src.ui.ui_menu import UIMenuBar
 from src.ui.ui_colorpicker import UIColorPicker
+from src.ui.ui_spinbox import UISpinBox
 from src.ui.constants import Anchors
