8e43b0ac6ebda0766f890fb68a2ecf961020eea5
Justus <justus.d2025@gmail.com>
2025-11-16 02:39:45 +0100

    add colorpicker & anchors

--- src/ui/utils.py
+++ src/ui/utils.py
@@ -7,3 +7,5 @@ from src.ui.ui_element import UIElement
 from src.ui.ui_textinput import UITextInput
 from src.ui.ui_dropdown import UIDropDown
 from src.ui.ui_menu import UIMenuBar
+from src.ui.ui_colorpicker import UIColorPicker
+from src.ui.constants import Anchors
