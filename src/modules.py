16d84996e1761592ae1f7b0969b50705f7890079
Justus <justus.d2025@gmail.com>
2025-11-19 12:38:18 +0100

    refac: modules

--- src/modules.py
+++ src/modules.py
@@ -1,11 +1,10 @@
 from pygame import Color, Surface, Vector2, Rect
-
 import pygame as pg
+
 import json, os
 import random
 from time import time
 pg.font.init()
 
-
-
 from typing import Callable
+from src.ui.utils import *
