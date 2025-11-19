58b803f5c036fafa4bf7f65978add771fc1bfef5
Justus <justus.d2025@gmail.com>
2025-11-19 12:40:32 +0100

    removed old utils & use pig_ui

--- src/ui/utils.py
+++ src/ui/utils.py
@@ -1,14 +1,20 @@
-from src.ui.ux_element import (
-    UXWrapper, 
-    UXText, 
-    UXRect
+from pig_ui import (
+    UIElement,
+    UIDropDown,
+    UITextInput,
+    UIColorPicker,
+    UIMenuBar,
+    UISideBar,
+    UISpinBox,
+    Anchors,
+    
+    UXWrapper,
+    UXText,
+    UXRect,
+    UXCircle,
+    UXImage,
+    
+    App
 )
-from src.ui.ui_element import UIElement
-from src.ui.ui_textinput import UITextInput
-from src.ui.ui_dropdown import UIDropDown
-from src.ui.ui_menu import UIMenuBar
-from src.ui.ui_colorpicker import UIColorPicker
-from src.ui.ui_spinbox import UISpinBox
-from src.ui.ui_sidebar import UISideBar
+
 from src.ui.ui_node import UINode
-from src.ui.constants import Anchors
