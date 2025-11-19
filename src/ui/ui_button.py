d0cfc6cea3f2d1532e2a08113d27259199b51d87
Justus <justus.d2025@gmail.com>
2025-11-19 12:38:45 +0100

    rem: button

--- src/ui/ui_button.py
+++
@@ -1,6 +0,0 @@
-from src.ui.ui_element import UIElement
-
-class UIButton(UIElement):
-    def __init__(self, app, pos, size, **kwargs):
-        super().__init__(app, pos, size, **kwargs)
-        
